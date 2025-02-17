324a3a546bd1a180d5386d30434e71c049b79398
Friedel van Megen <fmegen@microsoft.com>
2025-02-18 00:25:12 +0100

    tweak min min

--- core/keys/src/main/kotlin/app/aaps/core/keys/UnitDoubleKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/UnitDoubleKey.kt
@@ -20,7 +20,7 @@ enum class UnitDoubleKey(
     OverviewLowMark("low_mark", 72.0, 25, 160, showInNsClientMode = false, hideParentScreenIfHidden = true),
     OverviewHighMark("high_mark", 180.0, 90, 250, showInNsClientMode = false),
     OverviewVeryHighMark("very_high_mark", 400.0, 200, 400, showInNsClientMode = false),
-    OverviewVeryLowMark("very_low_mark", 55.0, 40, 80, showInNsClientMode = false),
+    OverviewVeryLowMark("very_low_mark", 55.0, 40, 160, showInNsClientMode = false),
     ApsLgsThreshold("lgsThreshold", 65.0, 60, 100, defaultedBySM = true, dependency = BooleanKey.ApsUseDynamicSensitivity),
     ApsAutoIsfSmbDeliveryRatioBgRange("openapsama_smb_delivery_ratio_bg_range", 0.0, 0, 100, defaultedBySM = true)
 }
